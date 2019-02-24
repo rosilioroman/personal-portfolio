f53c8d154d91fd4e70fa7608dcdc83497f2c3fd2
rosilioroman <rosilioroman@gmail.com>
2019-02-24 16:03:10 -0700

    Initial SideNav structure.

--- portfolio/src/Components/Navigation/SideNav.js
+++ portfolio/src/Components/Navigation/SideNav.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Navigation.css';
 
 const SideNav = props => {
     return(
-        <section className="side-nav" id="side-nav">
-            <nav>
-                <a href="#welcome">Welcome</a>
-                <a href="#about">About Me</a>
-                <a href="#work">Work</a>
-                <a href="#contact">Get In Touch</a>
+        <section className="side-nav">
+            <nav className="nav-container">
+                <a href="#welcome" className="navlink">Welcome</a>
+                <a href="#about" className="navlink">About Me</a>
+                <a href="#projects" className="navlink">Projects</a>
+                <a href="#contact" className="navlink">Get In Touch</a>
             </nav>
         </section>
     );
